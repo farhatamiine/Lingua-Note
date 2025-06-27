bb0d7f39e0b0b5cbe06287d03a7132face20beb9
farhatamiine <farhatamiine@gmail.com>
2025-06-27 02:10:20 +0200

    Remove custom Webpack configuration from next.config.ts, simplifying the setup for the Next.js application.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,5 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Prevent Webpack from crawling outside the project
-    config.snapshot = {
-      managedPaths: [/^(.next|node_modules)/],
-    };
-
-    // Skip problematic system folders if globbing happens
-    config.module.rules.push({
-      test: /\.ts$/,
-      loader: "ignore-loader",
-      include: [
-        "C:\\Users\\farha\\Cookies",
-        "C:\\Users\\farha\\Application Data",
-      ],
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
